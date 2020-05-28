5758ac282e99b7c26cd75eab1a9a23ac4d8cb68b
dewomser <webmaster@untergang.de>
2020-05-28 08:19:18 +0200

    Neues Radio für den Linux Desktop

---
+++ radiox1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+rm  fifo.txt
+touch fifo.txt
+
+
+radiostation()          {
+                      mpg123 --control --utf8 --title --preload 1 --buffer 768 --smooth "$1" >> fifo.txt  2>&1> /dev/null &
+                      result=$(tail -n 40 fifo.txt|grep -a --line-buffered "StreamTitle"| sed -e 's/;.*//' -e 's/.*=//' -e "s/'//g")
+                      station=$1
+                      kdialog --title "Radio Info" --passivepopup "$result" 10
+                      
+                      }
+                      
+        
+        
+        
+       
+    while true; do   
+       
+    wahl=$(kdialog --title "K+Radio" --radiolist "$station" 1 "Dot FM 8072" off  2  "Dot FM 8042" off 3 "SWR3" off)
+    
+    onoff=$?
+    
+    if [ $onoff -eq 1 ]; then
+    killall mpg123
+    exit
+    elif [ $onoff -eq 0 ]; then
+    killall mpg123
+    fi
+    
+    
+    
+    
+    if [ "$wahl" = 3 ]; then
+    radiostation "http://swr-swr3-live.cast.addradio.de/swr/swr3/live/mp3/128/stream.mp3" 
+    elif [ "$wahl" = 2 ]; then
+    radiostation http://relay.181.fm:8042
+    elif [ "$wahl" = 1 ]; then
+    radiostation http://relay.181.fm:8072 
+    
+    
+    fi
+
+    done
+    
+    
+    
